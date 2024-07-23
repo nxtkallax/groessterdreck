92f9a955b01bfc5af7a0fbbd451fef1dcaa2dd37
nxtkallax <areer@outlook.jp>
2024-07-23 21:14:35 +0200

    Update and rename scripts.js to scripts.lua

---
+++ scripts.lua
@@ -0,0 +1,21 @@
+-- Zufallsindex und Bild setzen
+local bilder = {
+    "/dreck/img/kippenfisch.jpg",
+    "/dreck/img/toshinokyouko.png"
+}
+local zufallsIndex = math.random(#bilder)
+local randomImageElement = document:getElementById("randomImage")
+randomImageElement.src = bilder[zufallsIndex]
+
+-- Funktion zum Abspielen des Sounds
+function playSound()
+    local image = document:getElementById("randomImage")
+    local currentSrc = image.src
+    if currentSrc:find("toshinokyouko.png") then
+        local audio = document:getElementById("toshinokyouko")
+        audio:play()
+    else
+        local audio = document:getElementById("yeehaw")
+        audio:play()
+    end
+end
